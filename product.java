abdb0f137f7efec61bf03014a9115e081409545e
Harikrishna <98644822+Harikrishna-kvn@users.noreply.github.com>
2023-12-15 13:43:15 +0530

    Update product.java

--- product.java
+++ product.java
@@ -1,1 +1,18 @@
 
+package com.example.shopping.model;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private double price;
+
+    // Constructors, getters, and setters (omitted for brevity)
+
+    // Example constructor:
+    public Product(Long id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
