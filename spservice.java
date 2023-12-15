0a90ec2a68d982ff151c8d6533e6ea5951178117
Harikrishna <98644822+Harikrishna-kvn@users.noreply.github.com>
2023-12-15 13:44:01 +0530

    Update spservice.java

--- spservice.java
+++ spservice.java
@@ -1,1 +1,22 @@
 
+package com.example.shopping.service;
+
+import com.example.shopping.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ShoppingCartService {
+
+    private List<Product> productList = new ArrayList<>();
+
+    // Constructor, methods to manage products (omitted for brevity)
+
+    // Example method:
+    public List<Product> getAllProducts() {
+        // Logic to retrieve all products (can be from a database, etc.)
+        return productList;
+    }
+}
