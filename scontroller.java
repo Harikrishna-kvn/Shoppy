85e50346154d53e0285bd4f3ea5a533224ec0978
Harikrishna <98644822+Harikrishna-kvn@users.noreply.github.com>
2023-12-15 13:36:40 +0530

    Update scontroller.java

--- scontroller.java
+++ scontroller.java
@@ -1,1 +1,16 @@
 
+package com.example.shopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShoppingCartController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        // You can add backend logic here
+        return "index";
+    }
+}
