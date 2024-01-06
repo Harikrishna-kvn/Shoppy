55d8e4dd839949799c05030059de5d7b482dc23a
Harikrishna <98644822+Harikrishna-kvn@users.noreply.github.com>
2024-01-06 18:45:59 +0530

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'mydatabase',
+    'host': 'mongodb://mongo:27017/mydatabase'
+}
+db = MongoEngine(app)
+
+class User(db.Document):
+    username = db.StringField(required=True, unique=True)
 
 @app.route('/')
-def hello():
-    return 'Hello, Dockerized Flask App!'
+def index():
+    users = User.objects()
+    return render_template('index.html', users=users)
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    username = request.form['username']
+    new_user = User(username=username)
+    new_user.save()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
